8f35fb0ef081c07719f16857e2a667878986d232
unknown <ppalesa85@gmail.com>
2022-01-26 10:31:57 +0200

    const pin added

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ function Submit(){
 
 }
  const text= text (document.getElementById("b").value);
- const unclock= unclock(document.getElementById("b").value)
+ const pin= pin(document.getElementById("b").value)
 
  if (text  == "") {
     alert("Email")
@@ -15,4 +15,13 @@ function Submit(){
  }
 
     document.getElementById('response').innerHTML = text;
+
+{
+    const text= text (document.getElementById("b").value);
+    const pin= pin(document.getElementById("b").value)
+   
+    if (pin  == "") {
+       alert("pin")
+       return;   
+}
  
