bd7d4cfd00813b75b8dc4705de39f53b3b5c11d1
unknown <ppalesa85@gmail.com>
2022-01-28 10:41:45 +0200

    login access added

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-let people = ["Precious", "26", "Mmapeaska", "29", "Andisiwe", "28", "Happy", "19", "Kagisho", "29", "Mdudzi", "55", "Sophie", "30", "Luck", "33", "Vusi", "22", "Wandile", "62", "khumbulani", "31", "Selina", "27", "Nkosiphendule", "31", "Mhnlonipheni", "21", "Sithembiso", "33"
+let people = ["Precious","26", "Mmapeaska", "29", "Andisiwe", "28", "Happy", "19", "Kagisho", "29", "Mdudzi", "55", "Sophie", "30", "Luck", "33", "Vusi", "22", "Wandile", "62", "khumbulani", "31", "Selina", "27", "Nkosiphendule", "31", "Mhnlonipheni", "21", "Sithembiso", "33"
 ];
 
 let display = document.getElementById('display');
